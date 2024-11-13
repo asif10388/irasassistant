3bcb14effcdc3158530f6e77008dfd4314943a83
Asif Hossain Khan <asif.khan10388@gmail.com>
2024-11-13 20:44:23 +0600

    added try catch to oauth-login redirection from redirect page

--- src/app/oauth/redirect/page.tsx
+++ src/app/oauth/redirect/page.tsx
@@ -11,18 +11,24 @@ const OauthCallbackPage = () => {
   useEffect(() => {
     async function handleCallback() {
       const cbUrl = "/api/oauth/oauth-callback";
+      const oauthUrl = "/api/oauth/oauth-login";
       const params = new URLSearchParams(window.location.hash.substring(1));
 
       const idToken = params.get("id_token");
       const accessToken = params.get("access_token");
 
       if (accessToken && idToken) {
-        const res = await axios.get(`${cbUrl}?access_token=${accessToken}&id_token=${idToken}`);
+        try {
+          const res = await axios.get(`${cbUrl}?access_token=${accessToken}&id_token=${idToken}`);
 
-        if (res?.data?.reAuthenticate) {
-          window.location.href = `/api/oauth/oauth-login?flow=login&login_hint=${res.data.login_hint}`;
-        } else {
-          router.push("/dashboard");
+          if (res?.data?.reAuthenticate) {
+            window.location.href = `${oauthUrl}?flow=login&login_hint=${res.data.login_hint}`;
+          } else {
+            router.push(process.env.NEXT_PUBLIC_REDIRECT_URL_DASHBOARD!);
+          }
+        } catch (error) {
+          console.error("Error handling callback: ", error);
+          router.push(process.env.NEXT_PUBLIC_REDIRECT_URL_LOGIN!);
         }
       }
     }
